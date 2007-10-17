3cc7f982cbf6ae66e4d341f97f8d670b99b3e658
H. Westphal <westphal@secure-net-concepts.de>
2007-10-17 08:15:56 +0000

    changed symfony paths for svn:external use

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 // symfony directories
-$sf_symfony_lib_dir  = 'C:\Program Files\xampp\php\pear/symfony';
-$sf_symfony_data_dir = 'C:\php5\pear\data/symfony';
+$sf_symfony_lib_dir  = dirname(__FILE__).'/../lib/vendor/symfony/lib';
+$sf_symfony_data_dir = dirname(__FILE__).'/../lib/vendor/symfony/data';
