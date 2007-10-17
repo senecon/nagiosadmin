c4a4ef838c7f903ce40e7d65e987f059501567e9
H. Westphal <westphal@secure-net-concepts.de>
2007-10-17 10:18:17 +0000

    delete protection for required templates implemented

--- lib/model/Template.php
+++ lib/model/Template.php
@@ -9,4 +9,28 @@
  */ 
 class Template extends BaseTemplate
 {
+
+  protected $template_names = array(
+    'command',
+    'contact',
+    'contactgroup',
+    'generic-contact',
+    'generic-host',
+    'generic-service',
+    'host',
+    'hostgroup',
+    'service'
+  );
+  
+  public function delete($con = null)
+  {
+    if (in_array($this->getName(),$this->template_names))
+    {
+      throw new PropelException("This Template is needed by nagios admin.");
+    }
+    else
+    {
+      parent::delete($con);
+    }
+  }
 }
