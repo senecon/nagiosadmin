5eafb10e7e17fbe33cd7483f6e88287b45c321a9
H. Westphal <westphal@secure-net-concepts.de>
2007-10-23 14:58:21 +0000

    display error message if at least one generated file has a size of 0 bytes

--- apps/backend/modules/generator/templates/dumpSuccess.php
+++ apps/backend/modules/generator/templates/dumpSuccess.php
@@ -1,3 +1,3 @@
 <?php include_partial('generator_header', array()) ?>
-<p>Configuration saved successfully.</p>
+<p>Error while saving configuration files. Be sure that there are no empty entrys in the generator view!</p>
 <?php include_partial('generator_footer', array()) ?>
