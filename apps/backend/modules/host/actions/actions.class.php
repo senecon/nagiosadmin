a9d7b7ac3f2de83906a1cd5890afd966a2bc4e8d
H. Westphal <westphal@secure-net-concepts.de>
2007-10-23 12:41:29 +0000

    removed PEAR dependency

--- apps/backend/modules/host/actions/actions.class.php
+++ apps/backend/modules/host/actions/actions.class.php
@@ -21,68 +21,59 @@ class hostActions extends autohostActions
       $host = $this->getHostOrCreate();
       $ip = $host->getAddress();
     }
-    
+
     // services with port != null
     $c = new Criteria();
     $c->add(ServicePeer::PORT,0,Criteria::GREATER_THAN);
     $services = ServicePeer::doSelect($c);
-    
-    require_once 'PEAR/Info.php';
-    
-    if(PEAR_Info::packageInstalled('Net_Portscan'))
+
+    require_once('Net/Portscan.php');
+    $ps = new Net_Portscan();
+
+    $this->services = array();
+    $ports = array();
+    foreach($services as $service)
     {
-      require_once('Net/Portscan.php');
-      $ps = new Net_Portscan();
-      
-      $this->services = array();
-      $ports = array();
-      foreach($services as $service)
+      if(!array_key_exists($service->getPort(), $ports))
       {
-        if(!array_key_exists($service->getPort(), $ports))
-        {
-          $ports[$service->getPort()] = $ps->checkPort($ip,$service->getPort(),5);
-        }
-        if($ports[$service->getPort()])
-        {
-          $this->services[] = 'associated_services_'.$service->getId();
-        }
+        $ports[$service->getPort()] = $ps->checkPort($ip,$service->getPort(),5);
       }
-      $this->getResponse()->setHttpHeader("X-JSON", '('.json_encode(array('services' => $this->services)).')');
+      if($ports[$service->getPort()])
+      {
+        $this->services[] = 'associated_services_'.$service->getId();
+      }
     }
-    else
-    {
-      $this->getResponse()->setHttpHeader("X-JSON", '('.json_encode(array('error' => 'PEAR Package Net_Portscan is missing.')).')');
-    }
-    
+    $this->getResponse()->setHttpHeader("X-JSON", '('.json_encode(array('services' => $this->services)).')');
+
     return sfView::HEADER_ONLY;
   }
-  
+
   public function executeServiceparameters()
   {
     $this->host = $this->getHostOrCreate();
     $this->services = $this->host->getServices();
   }
-  
+
   public function executeSaveserviceparameters()
   {
     $host = $this->getHostOrCreate();
-    
+
     foreach($this->getRequestParameter('sp') as $id => $params)
     {
-        $s2h = HostServiceParamPeer::retrieveByPK($host->getId(),$id);
-        if($s2h == null)
-        {
-           $s2h = new HostServiceParam();
-           $s2h->setHostId($host->getId());
-           $s2h->setServiceId($id);
-        }
-        $s2h->setParameter($params['param']);
-        $s2h->setSpecial($params['special']);
-        $s2h->save();
+      $s2h = HostServiceParamPeer::retrieveByPK($host->getId(),$id);
+      if($s2h == null)
+      {
+        $s2h = new HostServiceParam();
+        $s2h->setHostId($host->getId());
+        $s2h->setServiceId($id);
+      }
+      $s2h->setParameter($params['param']);
+      $s2h->setSpecial($params['special']);
+      $s2h->save();
     }
-    
+
     $this->setFlash('notice', 'Your modifications have been saved');
-    
+
     if($this->getRequestParameter('save')) $this->redirect('host/serviceparameters?id='.$host->getId());
     if($this->getRequestParameter('save_and_list')) $this->redirect('host/list?id='.$host->getId());
   }
