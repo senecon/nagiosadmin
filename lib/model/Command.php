49ac86933fc544b912b746769e44a5b8447b8e94
H. Westphal <westphal@secure-net-concepts.de>
2007-10-24 11:01:35 +0000

    cropped command string in list view

--- lib/model/Command.php
+++ lib/model/Command.php
@@ -13,4 +13,9 @@ class Command extends BaseCommand
   {
     return $this->getAlias();
   }
+  
+  public function getCommandSummary()
+  {
+    return strlen($this->getCommand()) > 60 ? substr($this->getCommand(),0,60).'...' : $this->getCommand();
+  }
 }
